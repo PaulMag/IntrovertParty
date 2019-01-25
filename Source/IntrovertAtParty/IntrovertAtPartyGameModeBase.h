ca1c1183c47362e98c237b06daf0f5c0223938ff
Eirik J. Henriksen <eirikjhen@gmail.com>
2019-01-26 00:45:54 +0100

    Implementerte watch. Lagde blueprint for spiller og gamemode.

--- Source/IntrovertAtParty/IntrovertAtPartyGameModeBase.h
+++ Source/IntrovertAtParty/IntrovertAtPartyGameModeBase.h
@@ -13,5 +13,37 @@ UCLASS()
 class INTROVERTATPARTY_API AIntrovertAtPartyGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+	AIntrovertAtPartyGameModeBase();
+
+	void Tick(float DeltaSeconds);
+
+public:
+	///////////////////////////////
+	// ==========|TIME|===========
+
+	void incrementTime(float DeltaSeconds);
+
+	float timeScale;
+
+	float maxHours;
+
+	float seconds;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Time")
+		float minutes;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Time")
+		float hours;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Time")
+		FString stringMinutes;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Time")
+		FString stringHours;
+
+	///////////////////////////////////////
+	// ==========|Game Systems|===========
+
+	bool gameOver;
 };
